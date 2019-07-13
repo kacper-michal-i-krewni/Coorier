6479602bbe532f23159aadd4433c7ca2ad7fb181
Michal <szpunar98@gmail.com>
2019-07-13 18:55:55 +0200

    Added basic API acces

---
+++ Models/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Coorier.Models
+{
+    public class Response
+    {
+        public string getResponse()
+        {
+            string url = String.Format("https://api.spotify.com/v1/albums/1/tracks");
+            WebRequest webRequest = WebRequest.Create(url);
+           
+            webRequest.Method = "GET";
+            HttpWebResponse httpResponse = null;
+            httpResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            string result = null;
+            using (Stream stream = httpResponse.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                result = sr.ReadToEnd();
+
+            }
+            
+            return result;
+     
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TestProject
+namespace Coorier
 {
     public class Program
     {
